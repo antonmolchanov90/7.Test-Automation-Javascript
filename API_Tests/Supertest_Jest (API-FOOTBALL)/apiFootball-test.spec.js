8e246c9082f941b3c1cb3d271dc3bdb89d6f27cc
Anton Molchanov <36717596+antonmolchanov90@users.noreply.github.com>
2020-09-06 10:49:40 +0600

    Create apiFootball-test.spec.js

---
+++ API_Tests/Supertest_Jest (API-FOOTBALL)/apiFootball-test.spec.js
@@ -0,0 +1,20 @@
+const request = require("supertest");
+
+describe('Test Footbal endpoints', () => {
+    
+    it('Should fail test prediction with 401', async () => {
+      const response = await request('https://api-football-v1.p.rapidapi.com')
+      .get('/v2/predictions/157462')
+      expect(response.body).toHaveProperty('message')
+      expect(response.status).toBe(401)
+    });
+    it('Should fail test football timezone with 403', async () => {
+      const response = await request('https://api-football-v1.p.rapidapi.com')
+      .get('/v2/timezone')
+      .set({ "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+      "x-rapidapi-key": "ab8b1ef81dmshb0f5e0f5b544304p164b50jsn83c454ad6bbb",
+      "useQueryString": true })
+      expect(response.status).toBe(403)
+      expect(response.body).toHaveProperty('message')
+    });
+});
